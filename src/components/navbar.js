6ebaa7bbc07e4b9bf2f363336a7a55385498498e
tahmdev <qwertiimusic@gmail.com>
2022-05-09 17:17:43 +0200

    Added light mode

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react"
+import useLocalstorage from "../hooks/useLocalStorage"
+
 const Navbar = ({setQuery, query ,setType, type, setShowInput, showInput, setShowAbout, showExamples, setShowExamples}) => {
+
+  let [lightmode, setLightmode] = useLocalstorage("lightmode", false)
 
   const handleRadical = () => {
     if (showInput=== "radical") {
@@ -30,25 +35,58 @@ const Navbar = ({setQuery, query ,setType, type, setShowInput, showInput, setSho
       )
     }
   } 
+
+  const handleLightmode = (e) => {
+    if(e.target.checked){
+      setLightmode(true)
+    }else{
+      setLightmode(false)
+    }
+  }
+
+  useEffect(() => {
+    let root = document.documentElement;
+    if(lightmode){
+      root.style.setProperty("--bg-100", "white")
+      root.style.setProperty("--bg-200", "rgb(226, 226, 226)")
+      root.style.setProperty("--bg-300", "rgb(197, 197, 197)")
+      root.style.setProperty("--primary-color-600", "rgb(89, 131, 236)")
+      root.style.setProperty("--text-color", "black")
+    }else{
+      root.style.setProperty("--bg-100", "rgb(43, 43, 43)")
+      root.style.setProperty("--bg-200", "rgb(70, 70, 70)")
+      root.style.setProperty("--bg-300", "rgb(122, 122, 122)")
+      root.style.setProperty("--primary-color-600", "rgb(89, 248, 195)")
+      root.style.setProperty("--text-color", "white")
+    }
+  }, [lightmode])
   return(
     <nav className='flex-center'>
       
-      <div>
-        <button className="input-method-button" 
-          onClick={handleRadical} 
-          style={activeStyle(showInput === "radical")}
-        >
-          <div>部</div>
-          <div className="button-desc">Radical</div>
-        </button>
-        <button className="input-method-button" 
-          onClick={handleHandwriting}
-          style={activeStyle(showInput === "handwriting")}  
-        >
-          <div>✏️</div>
-          <div className="button-desc">Draw</div>
-        </button>
-        <button className="small-only" onClick={() => setShowExamples(prev => !prev)}> {showExamples ? "Definition" : "Examples"} </button>
+      <div className="about-settings-wrapper">
+        <input className="lightmode-input" onChange={handleLightmode} checked={lightmode} type="checkbox" id="lightmode"/>
+        <label className="lightmode-label" htmlFor="lightmode" /> 
+        <button className="about-button" onClick={() => setShowAbout(true)} >About</button>
+      </div>
+
+      <div className="nav-button-wrapper">
+        <div>
+          <button className="input-method-button" 
+            onClick={handleRadical} 
+            style={activeStyle(showInput === "radical")}
+          >
+            <div>部</div>
+            <div className="button-desc">Radical</div>
+          </button>
+          <button className="input-method-button" 
+            onClick={handleHandwriting}
+            style={activeStyle(showInput === "handwriting")}  
+          >
+            <div>✏️</div>
+            <div className="button-desc">Draw</div>
+          </button>
+        </div>
+        <button className="small-only example-switch" onClick={() => setShowExamples(prev => !prev)}> {showExamples ? "Definition" : "Examples"} </button>
       </div>
 
       <div className="search-select-wrapper">
@@ -60,7 +98,6 @@ const Navbar = ({setQuery, query ,setType, type, setShowInput, showInput, setSho
           <option>Suffix</option>  
         </select>
       </div>
-      <button onClick={() => setShowAbout(true)} >About</button>
 
     </nav>
   )
